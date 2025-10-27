98b1fb3e296b24382a7e07e55b6b95cd096e4659
John Mola <mj66005@mail.harpercollege.edu>
2025-10-26 20:10:48 -0500

    Final Commit

--- src/point.cpp
+++ src/point.cpp
@@ -1,5 +1,23 @@
-#include <string>
+#include "point.hpp"
+#include <cmath>
 
-std::string hello() {
-   return "Hello World!";
+double Point::operator-(Point p1) const {
+   return sqrt(pow(x - p1.x, 2) + pow(y - p1.y, 2));
+}
+
+int Point::operator==(Point p1) const {
+   return x == p1.get_x() && y == p1.get_y();
+}
+
+int Point::operator!=(Point p1) const {
+   return x != p1.get_x() || y != p1.get_y();
+}
+
+Point Point::operator/(Point p1) const {
+   return Point((x+p1.get_x())/2, (y+p1.get_y())/2);
+}
+
+std::ostream& operator<<(std::ostream& os, const Point& p1) {
+   os << "(" << p1.get_x() << ", " << p1.get_y() << ")";
+   return os;
 }
--- src/point.hpp
+++ src/point.hpp
@@ -1,3 +1,22 @@
+#pragma once
+
+#include <iostream>
 #include <string>
 
-std::string hello();
+class Point {
+private:
+    double x;
+    double y;
+
+public:
+    Point(double x, double y) : x(x), y(y) {}
+
+    double operator-(Point p1) const;
+    int operator==(Point p1) const;
+    int operator!=(Point p1) const;
+    Point operator/(Point p1) const;
+    friend std::ostream& operator<<(std::ostream& os, Point p1);
+
+    [[nodiscard]] double get_x() const {return x;}
+    [[nodiscard]] double get_y() const {return y;}
+};
